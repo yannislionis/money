da1737c76814f97644df33c1a39a7178ff793737
yannislionis <j_melt@yahoo.com>
2009-04-03 20:15:05 +0100

    Added description column migration

---
+++ db/migrate/20090403195500_add_description_transactions.rb
@@ -0,0 +1,9 @@
+class AddDescriptionTransactions < ActiveRecord::Migration
+  def self.up
+   add_column :transactions, :description, :string, :default => ''
+  end
+
+  def self.down
+    remove_column  :transactions, :description
+  end
+end
